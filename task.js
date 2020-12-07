6a031b8fdee536919c5512ca878963dab576c630
wardtooma1 <68230045+wardtooma1@users.noreply.github.com>
2020-12-07 19:28:23 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,18 @@
+function burgerMenu(meat, cheese,topping, salad, bacon, bread){
+     
+
+    meat = prompt('Please choose your type of Meat \nMedium\nRare ');
+    cheese = prompt('Please choose your type of Cheese \nSheddar\nHaloomi ');
+    topping = prompt('Please choose your type of Topping ');
+    salad = prompt('Please choose your type of Salad ');
+    bacon = prompt('Please choose your type of bacon ');
+ 
+    bread =   prompt('Please choose your type fo Bread ');
+ 
+    console.log(`Your Burger is ready with ${meat} meat and ${cheese} cheese and ${topping} topping and ${salad} salad and ${bacon} bacon and ${bread} bread. Guten Appetit `);
+ 
+ }
+ 
+ burgerMenu ();
+ 
+ 
